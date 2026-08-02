02e3a87292dc4fe3fc5f9be018fd8fae61ddba63
MaybeItsZhaa <rafelareksadar@gmail.com>
2026-08-02 16:46:07 +0700

    Fix allowFullScreen boolean type error for iframe

--- app/page.tsx
+++ app/page.tsx
@@ -796,7 +796,7 @@ const Contact = () => {
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
-                  allowFullScreen=""
+                  allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                   className="absolute inset-0"
